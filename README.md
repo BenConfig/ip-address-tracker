c9f5fabc8aa9eb78784bd60c2e80e130437cabdb
Ben King <btking1990@googlemail.com>
2022-01-22 21:03:56 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Frontend Mentor - IP Address Tracker solution
+
+This is a solution to the [IP Address Tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+## Screenshot
+
+### Mobile
+![](screenshots/screenshot-mobile.png)
+
+### Desktop
+![](screenshots/screenshot-desktop.png)
+
+## Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- JavaScript
+- IP Geolocation API by IPify
+- LeafletJS API
+
+## What I learned
+
+Over the last week or two I have been learning about how to retrieve data from APIs and how to display this data on my own pages. This is the first Frontend Mentor challenge which has given me the opportunity to utilise the power of APIs.
+
+I was a little overwhelmed to start with as I didn't know how I was supposed to add the map. I therefore began with what I thought I could manage; fetching the data for the IP, location, time zone and ISP using the IP Geolocation API by IPify. It was a simple case of copying the API link from the website, and pasting this into Postman to see how the JSON object was structured, then I could retrieve the necessary data and assign them to the relevant HTML elements like so: 
+
+```js
+IP.innerText = data.ip;
+SITUATION.innerText = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
+TIME_ZONE.innerText = 'UTC ' + data.location.timezone;
+ISP.innerText = data.isp;
+```
+
+I then had to tackle the problem of adding the map using LeafletJS. I went on the Leaflet website and read through their tutorial, plus I watched a YouTube video which demonstrated how to set up the map. To get it to work involves copying some lines of code from the website. This code probably does a lot of stuff behind the scenes that I don't understand. However, eventually I did get the map to show but I just needed to add the custom location marker. This involved copying and pasting a couple more lines of code, then using the IP Geolocation API again to get the latitude and longitude coordinates and pass them into a function. I'm really pleased that everything worked in the end because when I started the challenge I was quite doubtful.
+
+## Continued development
+
+I think I need to practise using APIs a bit more as I'm still not sure what I'm doing some of the time. Unfortunately there's only one other Frontend Mentor challenge at Intermediate difficulty that utilises an API which is a shame. So I will probably do some code-alongs on YouTube to get a little more practice.
---
+++ main.js
@@ -0,0 +1,72 @@
+// Get form elements
+const IP_SEARCH_FIELD = document.getElementById('ip-search');
+const IP_SEARCH_BTN = document.querySelector('.ip-search-btn');
+const ERROR_MESSAGE = document.querySelector('.error-message');
+
+// Get 'result' elements
+const IP = document.getElementById('ip');
+const SITUATION = document.getElementById('location');
+const TIME_ZONE = document.getElementById('time-zone');
+const ISP = document.getElementById('isp');
+
+let searchedIP; 
+let map;
+let ipIsInvalid = false;
+
+function loadIPInfo(e) {
+    e.preventDefault(); // Prevents form from submitting
+    searchedIP = IP_SEARCH_FIELD.value.trim(); // If empty string, user's own details will display
+
+    // Link to IP Geolocation API
+    fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_M6zB6Q33eEfpDsofNmXQsDtsDpW9T&ipAddress=${searchedIP}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.ip) { // If IP address is valid
+                // Pass in the 'Results' from the IP Geolocation API
+                IP.innerText = data.ip;
+                SITUATION.innerText = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
+                TIME_ZONE.innerText = 'UTC ' + data.location.timezone;
+                ISP.innerText = data.isp;
+                
+                // If a map already exists, remove it
+                map && map.remove();
+                
+                // Pass in the coordinates from the IP Geolocation API
+                map = L.map('map').setView([data.location.lat, data.location.lng], 13);
+                
+                // Reposition the Leaflet buttons to avoid possible overlap with 'results' element
+                map.zoomControl.setPosition('bottomright');
+
+                // Load the Leaflet map tiles
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+                // Link to the marker icon in our design
+                let myIcon = L.icon({iconUrl: 'images/icon-location.svg'});
+
+                // Let marker match the coordinates from the IP Geolocation API
+                L.marker([data.location.lat, data.location.lng], {icon: myIcon}).addTo(map);
+            }
+            else { // If IP address is invalid
+                ERROR_MESSAGE.classList.add('show');
+                IP_SEARCH_FIELD.setAttribute('aria-invalid', true);
+                IP_SEARCH_FIELD.setAttribute('aria-describedby', 'error-message');
+                ipIsInvalid = true;
+            }
+        });
+};
+
+// Run loadIPInfo on page load to show IP information of user
+document.addEventListener('DOMContentLoaded', loadIPInfo);
+
+// Run loadIPInfo when IP Search Button is clicked
+IP_SEARCH_BTN.addEventListener('click', loadIPInfo);
+
+// Remove values associated with IP error when user changes the value in the IP search field
+IP_SEARCH_FIELD.addEventListener('input', () => {
+    if (ipIsInvalid) {
+        ERROR_MESSAGE.classList.remove('show');
+        IP_SEARCH_FIELD.removeAttribute('aria-invalid');
+        IP_SEARCH_FIELD.removeAttribute('aria-describedby');
+        ipIsInvalid = false;
+    }
+});
